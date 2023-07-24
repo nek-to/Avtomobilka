78a1177407c4bd2335c42fc7b35ac0394d3a6edf
Nikolaj Gribok <kolyagribok@gmail.com>
2023-07-24 13:14:56 +0400

    Initiating commit

---
+++ Avtomobilka/AvtomobilkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvtomobilkaApp.swift
+//  Avtomobilka
+//
+//  Created by admin on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AvtomobilkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
