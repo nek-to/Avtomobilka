011e8faaa8e6866fd39120b9b213f6ad201f4c15
Nikolaj Gribok <kolyagribok@gmail.com>
2023-07-24 17:00:32 +0400

    Created a cell view for the list of machines

--- Avtomobilka/Views/CarsListCard.swift
+++ Avtomobilka/Views/CarsListCard.swift
@@ -1,20 +1,57 @@
-//
-//  CarsListCard.swift
-//  Avtomobilka
-//
-//  Created by admin on 24.07.2023.
-//
-
 import SwiftUI
 
 struct CarsListCard: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var image: String
+	var brand: String
+	var model: String
+	var year: Int
+	var transmission: String
+	
+	var body: some View {
+		VStack {
+			VStack {
+				AsyncImage(url: URL(string: image)) { image in
+					image
+						.resizable()
+						.scaledToFill()
+						.cornerRadius(15)
+						.padding(.all)
+				} placeholder: {
+					ProgressView()
+				}
+				
+				HStack {
+					VStack(alignment: .leading) {
+						Text("Бренд: \(brand)")
+							.padding(.bottom, 3)
+						
+						Text("Модель: \(model)")
+							.padding(.bottom, 3)
+					}
+					
+					Spacer()
+					
+					VStack(alignment: .trailing) {
+						Text("Год: \(String(year))")
+							.padding(.bottom, 3)
+						
+						Text("Трансмиссия: \(transmission)")
+							.padding(.bottom, 3)
+					}
+				}
+				.padding([.bottom, .horizontal])
+			}
+		}
+		.background(Color.white)
+		.cornerRadius(25)
+		.shadow(radius: 20)
+		.padding(.horizontal)
+	}
 }
 
+
 struct CarsListCard_Previews: PreviewProvider {
-    static var previews: some View {
-        CarsListCard()
-    }
+	static var previews: some View {
+		CarsListCard(image: "http://am111.05.testing.place/uploads/user/37/auto/49/fc40ee0a0dbf97b2e504b2f48438a8ba_w500.jpg", brand: "Volkswagen", model: "Tiguan", year: 2018, transmission: "AT")
+	}
 }
